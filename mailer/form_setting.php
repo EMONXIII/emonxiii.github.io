c3ad94b2d6238f5a9f9bbad2d2e50c712d82c557
Ismail hossan emon <emonnn@yahoo.com>
2024-11-19 21:19:11 +0600

    19/11/24

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Cvio - Resume/CV Template'"; //letter subject
+	$to = 'ismail.emonxii@gmail.com'; // Enter Your E-mail
+	$from = 'ismail.emonxii@gmail.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
